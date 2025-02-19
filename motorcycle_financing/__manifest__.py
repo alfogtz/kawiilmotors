7701ecd7758aca0a8cd2156e50286bf6ea2a9ef3
alfogtz <alfonsog.gutierreza@gmail.com>
2025-02-18 20:53:43 -0800

    adding motorcycle financing module

---
+++ motorcycle_financing/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Motorcycle Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+    'description': 'This module helps K\'awiil Motors track and manage motorcycle loan applications efficiently.',
+    'license': 'OPL-1',
+    'category': 'Kawiil/Custom Modules',
+    'author': 'alfogtz',  # O tu usuario de GitHub
+    'website': 'https://github.com/alfogtz/kawiilmotors',  # Ajusta con tu URL real
+    'version': '18.0.0.0.1',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
