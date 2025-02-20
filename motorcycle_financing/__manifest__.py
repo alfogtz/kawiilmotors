265a5e7ffefbe207c79fe9b75e034008621bfdb9
alfogtz <alfonsog.gutierreza@gmail.com>
2025-02-19 19:02:11 -0800

    Added window action for loan.application

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -8,7 +8,8 @@
     'website': 'https://github.com/alfogtz/kawiilmotors',  # Ajusta con tu URL real
     'version': '18.0.0.0.1',
     'depends': [],
-    'data': ['views/motorcycle_financing_menu.xml',],
+    'data': ['views/motorcycle_financing_menu.xml',
+             'views/loan_application_views.xml',],
     'demo': ['data/loan_demo.xml',],
     'application': True,
 }
