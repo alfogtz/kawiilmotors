498e44997e325c89370a8bc25b5f36cbd24c746a
alfogtz <alfonsog.gutierreza@gmail.com>
2025-02-19 19:52:55 -0800

    Menu and views fixed

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -4,8 +4,8 @@
     'description': 'This module helps K\'awiil Motors track and manage motorcycle loan applications efficiently.',
     'license': 'OPL-1',
     'category': 'Kawiil/Custom Modules',
-    'author': 'alfogtz',  # O tu usuario de GitHub
-    'website': 'https://github.com/alfogtz/kawiilmotors',  # Ajusta con tu URL real
+    'author': 'alfogtz',
+    'website': 'https://github.com/alfogtz/kawiilmotors',
     'version': '18.0.0.0.1',
     'depends': [],
     'data': ['views/motorcycle_financing_menu.xml',
