f4b2f930edf2feb6106c41760ce1f08332f81f0e
alfogtz <alfonsog.gutierreza@gmail.com>
2025-02-24 19:06:56 -0800

    Computed and relates fields added

--- motorcycle_financing/models/loan_application.py
+++ motorcycle_financing/models/loan_application.py
@@ -5,16 +5,13 @@ class LoanApplication(models.Model):
     _description = 'Loan Application'
 
     name = fields.Char(string="Application Number", required=True)
-    currency_id = fields.Many2one(
-        'res.currency', string="Currency",
-        default=lambda self: self.env.company.currency_id.id)
+    currency_id = fields.Many2one(related="sale_order_id.currency_id", readonly=True, store=True)
     date_application = fields.Date(string="Application Date", readonly=True, copy=False)
     date_approval = fields.Date(string="Approval Date", readonly=True, copy=False)
     date_rejection = fields.Date(string="Rejection Date", readonly=True, copy=False)
     date_signed = fields.Datetime(string="Signed On", readonly=True, copy=False)
     down_payment = fields.Monetary(string="Down Payment", required=True, currency_field='currency_id')
     interest_rate = fields.Float(string="Interest Rate (%)", required=True, digits=(5, 2))
-    loan_amount = fields.Monetary(string="Loan Amount", required=True, currency_field='currency_id')
     loan_term = fields.Integer(string="Loan Term (Months)", required=True, default=36)
     rejection_reason = fields.Text(string="Rejection Reason", copy=False)
     state = fields.Selection([
@@ -27,35 +24,34 @@ class LoanApplication(models.Model):
         ('cancel', 'Canceled'),
     ], string="Status", default='draft', copy=False)
     notes = fields.Html(string="Notes", copy=False)
+    document_ids = fields.One2many('loan.application.document','application_id',string='Documents')
+    tag_ids = fields.Many2many('loan.application.tag',string='Tags')
+    partner_id = fields.Many2one(related="sale_order_id.partner_id", readonly=True, store=True)
+    sale_order_id = fields.Many2one('sale.order',string='Related Sale Order')
+    user_id = fields.Many2one(related="sale_order_id.user_id", readonly=True, store=True)
+    product_template_id = fields.Many2one('product.template', string='Product')
+    sale_order_total = fields.Float(related="sale_order_id.amount_total", readonly=True, store=True)
 
-    document_ids = fields.One2many(
-        'loan.application.document',
-        'application_id',
-        string='Documents'
-    )
+    @api.depends("sale_order_total", "down_payment")
+    def _loan_amount(self):
+        for loan in self:
+            loan.loan_amount = loan.sale_order_total - loan.down_payment
 
-    tag_ids = fields.Many2many(
-        'loan.application.tag',
-        string='Tags'
-    )
+    loan_amount = fields.Monetary(string="Loan Amount", compute="_loan_amount_computation",
+                                  store=True, currency_field='currency_id'
+                                  )
 
-    partner_id = fields.Many2one(
-        'res.partner',
-        string='Customer'
-    )
+    @api.depends("document_ids")
+    def _compute_document_count(self):
+        for record in self:
+            record.document_count = len(record.document_ids)
 
-    sale_order_id = fields.Many2one(
-        'sale.order',
-        string='Related Sale Order'
-    )
+    document_count = fields.Integer(string="Document Count", compute="_compute_document_count", store=True)
 
-    user_id = fields.Many2one(
-        'res.users',
-        string='Salesperson'
-    )
+    @api.depends("document_ids", "document_ids.state")
+    def _compute_document_count_approved(self):
+        for record in self:
+            record.document_count_approved = len(record.document_ids.filtered(lambda d: d.state == "approved"))
 
-    product_template_id = fields.Many2one(
-        'product.template',
-        string='Product'
-    )
-
+    document_count_approved = fields.Integer(string="Approved Documents", compute="_compute_document_count_approved",
+                                             store=True)
