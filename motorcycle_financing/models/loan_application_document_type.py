79908cb8548bf3d3bc16aaae0d3395d5e94967b0
alfogtz <alfonsog.gutierreza@gmail.com>
2025-02-24 17:37:39 -0800

    Fixed tags field behavior

--- motorcycle_financing/models/loan_application_document_type.py
+++ motorcycle_financing/models/loan_application_document_type.py
@@ -5,9 +5,6 @@ class LoanApplicationDocumentType(models.Model):
     _description = 'Loan Application Document Type'
 
     name = fields.Char(string="Documents", required=True, unique=True)
-    #active = fields.Boolean(string="Tags", default=True)
-    tag_ids = fields.Many2many(
-        'loan.application.tag',  # Reference to the tags model
-        string="Tags"
-    )
+    active = fields.Boolean(string="Active", default=True)
+
     document_number = fields.Integer(string="Required Document Number")
