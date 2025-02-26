998626ad5d8edc037d6b820c6d9bb29d069a132d
alfogtz <alfonsog.gutierreza@gmail.com>
2025-02-25 18:22:16 -0800

    ADD: Approve, rejection buttons

--- motorcycle_financing/models/loan_application.py
+++ motorcycle_financing/models/loan_application.py
@@ -10,6 +10,7 @@ class LoanApplication(models.Model):
     date_approval = fields.Date(string="Approval Date", readonly=True, copy=False)
     date_rejection = fields.Date(string="Rejection Date", readonly=True, copy=False)
     date_signed = fields.Datetime(string="Signed On", readonly=True, copy=False)
+    date_sent = fields.Date(string="Date Sent", readonly=True, copy=False)
     down_payment = fields.Monetary(string="Down Payment", required=True, currency_field='currency_id')
     interest_rate = fields.Float(string="Interest Rate (%)", required=True, digits=(5, 2))
     loan_term = fields.Integer(string="Loan Term (Months)", required=True, default=36)
@@ -56,3 +57,37 @@ class LoanApplication(models.Model):
 
     document_count_approved = fields.Integer(string="Approved Documents", compute="_compute_document_count_approved",
                                              store=True)
+
+    #Sends app for approval but checking if documents are attached
+    def action_send_for_approval(self):
+        for loan in self:
+            if not loan.document_ids:
+                raise exceptions.ValidationError(
+                    "It seems that some documents are missing, you can not send to approval without documents attached.")
+
+            if any(doc.state != 'approved' for doc in loan.document_ids):
+                raise exceptions.ValidationError(
+                    "All documents must be in 'Approved' state before sending the loan for approval.")
+
+            loan.write({
+                'state': 'sent',
+                'date_sent': date.today()
+            })
+
+    #loan approval
+    def action_approve_loan(self):
+        self.write({
+            'state': 'approved',
+            'date_approval': date.today()
+        })
+
+    #Loan rejection
+    def action_reject_loan(self, rejection_reason):
+        if not rejection_reason:
+            raise exceptions.ValidationError("Please provide a reason for rejection.")
+
+        self.write({
+            'state': 'rejected',
+            'date_rejection': date.today(),
+            'rejection_reason': rejection_reason
+        })
