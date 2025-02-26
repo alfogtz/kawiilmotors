47bfc968dc1a848823ded32bf104110a3a8b014c
alfogtz <alfonsog.gutierreza@gmail.com>
2025-02-25 18:05:45 -0800

    Notebook added into loan form view

--- motorcycle_financing/models/loan_application_document_type.py
+++ motorcycle_financing/models/loan_application_document_type.py
@@ -6,5 +6,5 @@ class LoanApplicationDocumentType(models.Model):
 
     name = fields.Char(string="Documents", required=True, unique=True)
     active = fields.Boolean(string="Active", default=True)
-
+    type_id = fields.Many2one("loan.application.document.type", string="Document Type")
     document_number = fields.Integer(string="Required Document Number")
